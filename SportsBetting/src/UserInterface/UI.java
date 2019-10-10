61f258f245611f9085cdb8d03e3adccbb798c962
krujzg97 <47007011+krujzg97@users.noreply.github.com>
2019-10-10 16:48:42 +0200

    seperatedAppLogic

--- SportsBetting/src/UserInterface/UI.java
+++ SportsBetting/src/UserInterface/UI.java
@@ -5,42 +5,8 @@ import Exceptions.OutcomeOddTimeOverlapException;
 import Repository.Interfaces.ISportsBettingService;
 import Repository.Interfaces.IView;
 
-public class App {
+public class UI {
 
-   
-    
-    public App(ISportsBettingService sportbettingservice,IView view) 
-    {
-        
-        
-        
-    }
-    
-    public void play()
-    {
-        
-    }
-    
-    public void createPlayer(){
-        
-    }
-    
-    public void doBetting()
-    {
-        
-    }
-    
-    public void calculateResults()
-    {
-        
-    }
-    
-    public void printResults()
-    {
-        
-    }
-
-    
     public static void main(String[] args) {
        
         try
